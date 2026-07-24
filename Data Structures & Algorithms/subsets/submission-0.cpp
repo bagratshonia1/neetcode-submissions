ee65babc6e1c7b98eae681e6b7e55b91e6682be0
bagratshonia1 <146414964+bagratshonia1@users.noreply.github.com>
2026-07-25 03:39:50 +0400

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void helper(vector<int>& nums, vector<vector<int>>& result, vector<int>& current, int start){
+        if(current.size() == nums.size()){
+            return;
+        }
+        for(int i = start; i < nums.size(); i++){
+            current.push_back(nums[i]);
+            result.push_back(current);
+            helper(nums, result, current, i + 1);
+            current.pop_back();
+        }
+    }
+
+
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> current;
+        result.push_back({});    
+        helper(nums, result, current, 0);
+        return result;  
+    }
+};
