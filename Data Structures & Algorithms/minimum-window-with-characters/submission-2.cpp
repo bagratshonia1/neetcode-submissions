5fd89eb33cc216b655c90c500ed7cd5695043330
bagratshonia1 <146414964+bagratshonia1@users.noreply.github.com>
2026-08-01 01:52:02 +0400

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    //checks if map_1 contains everything from map_2
+    bool isSubMap(unordered_map<char, int>& processed,
+     unordered_map<char, int>& tMap){
+        for(const auto& [key, value] : tMap){
+            //find() returns an iterator to the key/value pair
+            auto it = processed.find(key);
+            if(it == processed.end() || it->second < value){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    string minWindow(string s, string t) {
+        unordered_map<char, int> tMap;
+        unordered_map<char, int> processed;
+        for(int i = 0; i < t.length(); i++){
+            tMap[t[i]]++;
+        }
+        int minLength = INT_MAX;
+        int start = 0;
+        int left = 0;
+        int right = 0;
+        while(right < s.length()){
+            processed[s[right]]++;
+            while(isSubMap(processed, tMap)){
+                if(right - left + 1 < minLength){
+                    minLength = right - left + 1;
+                    start = left;
+                }
+                processed[s[left]]--;
+                left++;
+            }
+            right++;
+        }
+        return minLength == INT_MAX ? "" : s.substr(start, minLength);
+    }
+};
